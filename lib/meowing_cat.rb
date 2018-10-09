44b0196d3b8dfc2102ce1287ba8ae1d717b71868
Austin Luft <ausluft@gmail.com>
2018-10-09 18:25:56 +0000

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,16 @@
+require "pry"
+
 ## code your solution here. 
+class Cat
+  
+# def initialize(name)
+#   @name = name
+# end
+  
+attr_accessor :name
+
+def meow
+  puts "meow!"
+end
+
+end #class Cat 
